3a867af90f68ae1ed2048936e73120b9a18767fd
Brian Demro <demrobrian@hotmail.com>
2020-10-21 17:21:25 -0500

    all necessary updating

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      crazyIdeas: ["finger guns", "dog cell phones", "mini computers", 
+      "edible casings with ground up meat inside", "edible culturized milk products"]
+    };
+  }
+
+  render() {
+    let ideasToDisplay = this.state.crazyIdeas.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+
+    return <div className="App">{ideasToDisplay}</div>;
+  }
 }
 
 export default App;
